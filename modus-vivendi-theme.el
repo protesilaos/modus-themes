62be3c0f9791c90504875f9fea00187181247619
Protesilaos Stavrou <info@protesilaos.com>
2019-12-04 22:44:16 +0200

    Minor tweaks to generic faces
    These make them less prone to breakage.  One example where a problem
    occurred was using `ivy-rich' and trying to switch buffers.  The extra
    columns created by `ivy-rich' would not inherit the values of Ivy's
    currently matched item.

--- modus-vivendi-theme.el
+++ modus-vivendi-theme.el
@@ -243,7 +243,7 @@ between foreground and background is >= 7:1)."
    `(fringe ((,class (:inherit default))))
    ;;;; basic and/or ungrouped styles
    `(diary ((,class (:foreground ,yellow))))
-   `(error ((,class (:background ,bg-main :foreground ,red-intense :weight bold))))
+   `(error ((,class (:foreground ,red :weight bold))))
    `(escape-glyph ((,class (:inherit modus-theme-refine-blue :weight bold))))
    `(header-line ((,class (:background ,bg-alt :foreground ,fg-alt))))
    `(homoglyph ((,class (:foreground ,yellow-alt-other))))
@@ -257,9 +257,9 @@ between foreground and background is >= 7:1)."
    `(mm-uu-extract ((,class (:inherit modus-theme-refine-yellow))))
    `(next-error ((,class (:inherit modus-theme-subtle-red))))
    `(shadow ((,class (:foreground ,fg-alt))))
-   `(success ((,class (:background ,bg-main :foreground ,green-intense :weight bold))))
+   `(success ((,class (:foreground ,green :weight bold))))
    `(trailing-whitespace ((,class (:background ,red-alt))))
-   `(warning ((,class (:background ,bg-main :foreground ,yellow-intense :weight bold))))
+   `(warning ((,class (:foreground ,yellow :weight bold))))
    ;;;; all-the-icons
    `(all-the-icons-blue ((,class (:foreground ,blue))))
    `(all-the-icons-blue-alt ((,class (:foreground ,blue-alt))))
