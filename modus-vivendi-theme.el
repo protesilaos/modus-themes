d44307abe05da589a4608d555b846790ec8a4557
Protesilaos Stavrou <info@protesilaos.com>
2019-12-18 17:53:15 +0200

    Add support for KEYCAST

--- modus-vivendi-theme.el
+++ modus-vivendi-theme.el
@@ -931,6 +931,9 @@ between foreground and background is >= 7:1)."
    `(ivy-posframe ((,class (:background ,bg-dim :foreground ,fg-main))))
    `(ivy-posframe-border ((,class (:background ,bg-active))))
    `(ivy-posframe-cursor ((,class (:inherit cursor))))
+   ;;;; keycast
+   `(keycast-command ((,class (:foreground ,red-active :weight bold))))
+   `(keycast-key ((,class (:height 1.2 :inherit modus-theme-special-warm :weight bold :box (:line-width -3 :style released-button)))))
    ;;;; line numbers (display-line-numbers-mode and global variant)
    `(line-number ((,class (:background ,bg-alt :foreground ,fg-alt))))
    `(line-number-current-line ((,class (:background ,bg-active :foreground ,fg-active :weight bold))))
