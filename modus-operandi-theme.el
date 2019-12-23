82e21d0b3e6822149ac2392d0e31f432906e74ae
Protesilaos Stavrou <info@protesilaos.com>
2019-12-23 06:58:15 +0200

    Update commentary

--- modus-operandi-theme.el
+++ modus-operandi-theme.el
@@ -32,12 +32,15 @@
 ;; accessibility criterion per the guidelines of the Worldwide Web
 ;; Consortium's Working Group on Accessibility (WCAG AAA standard).
 ;;
-;; 2. Offer as close to full face coverage as possible.  The output of
-;; `list-faces-display' offers all we need.  Note though, that it is
-;; difficult to create theme styles without actually using the
-;; packages/interfaces that require them.  As such, the development of
-;; this theme will be incremental, gradually incorporating the
-;; customisations for packages I use or have been exposed to.
+;; 2. Offer as close to full face coverage as possible.  The list is
+;; already quite long, with more additions to follow.
+;;
+;; Note though, that it is difficult to create theme styles without
+;; actually using the packages/interfaces that require them.  The output
+;; of `list-faces-display' offers all we need to get started.  As such,
+;; the development of this theme will be incremental, gradually
+;; incorporating the customisations for packages I use or have been
+;; exposed to.
 
 ;;; Code:
 
