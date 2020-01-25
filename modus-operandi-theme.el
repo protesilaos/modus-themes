protesilaos/modus-themes

commit 1ba8d82673cbb42ce48221ec1b0d12379e4faa4d

Review face inheritance to avoid possible bugs
The rationale for this change was already outlined in commit b02feae.
Basically the point is to be more careful with inheriting styles due
to the implicit dependencies they create.

Inheritance should only be limited to the custom `modus-*' faces that
are part of each theme.  From my perspective these are only known
constant in a user's setup.  One face group should not inherit from
another group because there may be cases where the relevant library is
not loaded.

An exception is made for basic faces such as "error" and "success",
though I will review those as well if the need arises.

modus-operandi-theme.el
@@ -392,8 +392,8 @@ between foreground and background is >= 7:1)."
    `(aw-mode-line-face ((,class (:weight bold))))
    ;;;; buttons, links, widgets
    `(button ((,class (:foreground ,blue-alt-other :underline t))))
-   `(link ((,class (:inherit button))))
-   `(link-visited ((,class (:inherit button :foreground ,magenta-alt-other))))
+   `(link ((,class (:foreground ,blue-alt-other :underline t))))
+   `(link-visited ((,class (:foreground ,magenta-alt-other :underline t))))
    `(tooltip ((,class (:inherit modus-theme-subtle-yellow))))
    `(widget-button ((,class (:inherit button))))
    `(widget-button-pressed ((,class (:inherit button :foreground ,magenta))))
@@ -459,9 +459,9 @@ between foreground and background is >= 7:1)."
    `(compilation-mode-line-run ((,class (:foreground ,magenta-active :weight bold))))
    `(compilation-warning ((,class (:foreground ,yellow :weight bold))))
    ;;;; completions
-   `(completions-annotations ((,class (:inherit italic))))
+   `(completions-annotations ((,class (:foreground ,fg-special-cold :slant ,modus-theme-slant))))
    `(completions-common-part ((,class nil)))
-   `(completions-first-difference ((,class (:inherit bold))))
+   `(completions-first-difference ((,class (:weight bold))))
    ;;;; counsel
    `(counsel-active-mode ((,class (:foreground ,magenta-alt-other))))
    `(counsel-application-name ((,class (:foreground ,red-alt-other))))
@@ -754,8 +754,8 @@ between foreground and background is >= 7:1)."
    ;;;; evil-mode
    `(evil-ex-commands ((,class (:foreground ,magenta-alt-other))))
    `(evil-ex-info ((,class (:foreground ,cyan-alt-other))))
-   `(evil-ex-lazy-highlight ((,class (:inherit lazy-highlight))))
-   `(evil-ex-search ((,class (:inherit isearch))))
+   `(evil-ex-lazy-highlight ((,class (:inherit modus-theme-refine-cyan))))
+   `(evil-ex-search ((,class (:inherit modus-theme-intense-green))))
    `(evil-ex-substitute-matches ((,class (:inherit modus-theme-refine-yellow :underline t))))
    `(evil-ex-substitute-replacement ((,class (:inherit modus-theme-intense-green :weight bold))))
    ;;;; evil-goggles
@@ -856,7 +856,7 @@ between foreground and background is >= 7:1)."
       (,class (:foreground ,red :underline t))))
    ;;;; fountain-mode
    `(fountain-character ((,class (:foreground ,magenta-alt-other))))
-   `(fountain-comment ((,class (:inherit font-lock-comment-face))))
+   `(fountain-comment ((,class (:foreground ,fg-alt :slant ,modus-theme-slant))))
    `(fountain-dialog ((,class (:foreground ,blue))))
    `(fountain-metadata-key ((,class (:foreground ,blue-alt-other))))
    `(fountain-metadata-value ((,class (:foreground ,cyan-alt-other))))
@@ -1236,7 +1236,7 @@ between foreground and background is >= 7:1)."
    `(markdown-blockquote-face ((,class (:background ,bg-dim :foreground ,fg-special-warm :slant ,modus-theme-slant))))
    `(markdown-bold-face ((,class (:weight bold))))
    `(markdown-code-face ((,class (:inherit fixed-pitch))))
-   `(markdown-comment-face ((,class (:inherit font-lock-comment-face))))
+   `(markdown-comment-face ((,class (:foreground ,fg-alt :slant ,modus-theme-slant))))
    `(markdown-footnote-marker-face ((,class (:foreground ,cyan-alt :weight bold))))
    `(markdown-footnote-text-face ((,class (:foreground ,fg-main :slant ,modus-theme-slant))))
    `(markdown-gfm-checkbox-face ((,class (:foreground ,cyan-alt-other))))
@@ -1324,7 +1324,7 @@ between foreground and background is >= 7:1)."
    `(mu4e-footer-face ((,class (:foreground ,fg-alt))))
    `(mu4e-forwarded-face ((,class (:foreground ,green-alt-other))))
    `(mu4e-header-face ((,class (:foreground ,fg-main))))
-   `(mu4e-header-highlight-face ((,class (:inherit hl-line))))
+   `(mu4e-header-highlight-face ((,class (:background ,bg-hl-line))))
    `(mu4e-header-key-face ((,class (:foreground ,fg-dim :weight bold))))
    `(mu4e-header-marks-face ((,class (:foreground ,magenta-alt :weight bold))))
    `(mu4e-header-title-face ((,class (:foreground ,fg-special-warm))))
@@ -1334,7 +1334,7 @@ between foreground and background is >= 7:1)."
    `(mu4e-modeline-face ((,class (:foreground ,magenta-active))))
    `(mu4e-moved-face ((,class (:foreground ,blue-refine-fg))))
    `(mu4e-ok-face ((,class (:foreground ,green-intense :weight bold))))
-   `(mu4e-region-code ((,class (:inherit show-paren-match-expression))))
+   `(mu4e-region-code ((,class (:inherit modus-theme-special-calm))))
    `(mu4e-replied-face ((,class (:foreground ,cyan-refine-fg))))
    `(mu4e-special-header-value-face ((,class (:foreground ,magenta :weight bold))))
    `(mu4e-system-face ((,class (:foreground ,fg-alt))))
@@ -1358,10 +1358,10 @@ between foreground and background is >= 7:1)."
    ;;;; neotree
    `(neo-banner-face ((,class (:foreground ,magenta))))
    `(neo-button-face ((,class (:inherit button))))
-   `(neo-dir-link-face ((,class (:inherit dired-directory))))
+   `(neo-dir-link-face ((,class (:foreground ,blue :weight bold))))
    `(neo-expand-btn-face ((,class (:foreground ,cyan))))
    `(neo-file-link-face ((,class (:foreground ,fg-main))))
-   `(neo-header-face ((,class (:inherit dired-header))))
+   `(neo-header-face ((,class (:foreground ,fg-main :weight bold))))
    `(neo-root-dir-face ((,class (:foreground ,cyan-alt :weight bold))))
    `(neo-vc-added-face ((,class (:foreground ,green))))
    `(neo-vc-conflict-face ((,class (:foreground ,red :Weight bold))))
@@ -1452,7 +1452,7 @@ between foreground and background is >= 7:1)."
    `(org-link ((,class (:inherit link))))
    `(org-list-dt ((,class (:foreground ,fg-dim :weight bold))))
    `(org-macro ((,class (:inherit org-latex-and-related))))
-   `(org-meta-line ((,class (:inherit font-lock-comment-face))))
+   `(org-meta-line ((,class (:foreground ,fg-alt :slant ,modus-theme-slant))))
    `(org-mode-line-clock ((,class (:background ,bg-main :foreground ,fg-main))))
    `(org-mode-line-clock-overrun ((,class (:inherit modus-theme-active-red))))
    `(org-priority ((,class (:foreground ,magenta))))
@@ -1601,8 +1601,8 @@ between foreground and background is >= 7:1)."
    `(ruler-mode-pad ((,class (:background ,bg-active :foreground ,fg-inactive))))
    `(ruler-mode-tab-stop ((,class (:inherit ruler-mode-default :foreground ,yellow-active))))
    ;;;; shell scripts
-   `(sh-heredoc ((,class (:inherit font-lock-string-face))))
-   `(sh-quoted-exec ((,class (:inherit font-lock-builtin-face))))
+   `(sh-heredoc ((,class (:foreground ,blue-alt))))
+   `(sh-quoted-exec ((,class (:foreground ,magenta-alt :weight ,modus-theme-bold))))
    ;;;; show-paren-mode
    `(show-paren-match ((,class (:background ,bg-paren-match :foreground ,fg-paren-match :weight bold))))
    `(show-paren-match-expression ((,class (:inherit modus-theme-special-calm))))
@@ -1636,8 +1636,8 @@ between foreground and background is >= 7:1)."
    ;;;; smartparens
    `(sp-pair-overlay-face ((,class (:inherit modus-theme-special-warm))))
    `(sp-show-pair-enclosing ((,class (:inherit modus-theme-special-mild))))
-   `(sp-show-pair-match-face ((,class (:inherit show-paren-match))))
-   `(sp-show-pair-mismatch-face ((,class (:inherit show-paren-mismatch))))
+   `(sp-show-pair-match-face ((,class (:background ,bg-paren-match :foreground ,fg-paren-match :weight bold))))
+   `(sp-show-pair-mismatch-face ((,class (:inherit modus-theme-intense-red))))
    `(sp-wrap-overlay-closing-pair ((,class (:inherit sp-pair-overlay-face :weight bold))))
    `(sp-wrap-overlay-face ((,class (:inherit sp-pair-overlay-face))))
    `(sp-wrap-overlay-opening-pair ((,class (:inherit sp-pair-overlay-face :weight bold))))
@@ -1653,9 +1653,9 @@ between foreground and background is >= 7:1)."
    `(smerge-upper ((,class (:inherit modus-theme-subtle-red))))
    ;;;; speedbar
    `(speedbar-button-face ((,class (:inherit link))))
-   `(speedbar-directory-face ((,class (:inherit dired-directory))))
+   `(speedbar-directory-face ((,class (:foreground ,blue :weight bold))))
    `(speedbar-file-face ((,class (:inherit default))))
-   `(speedbar-highlight-face ((,class (:inherit highlight))))
+   `(speedbar-highlight-face ((,class (:inherit modus-theme-subtle-blue))))
    `(speedbar-selected-face ((,class (:foreground ,cyan :weight bold))))
    `(speedbar-separator-face ((,class (:inherit modus-theme-intense-neutral))))
    `(speedbar-tag-face ((,class (:foreground ,yellow-alt-other))))
@@ -1751,13 +1751,13 @@ between foreground and background is >= 7:1)."
    `(treemacs-git-renamed-face ((,class (:foreground ,cyan-alt-other))))
    `(treemacs-git-unmodified-face ((,class (:foreground ,fg-main))))
    `(treemacs-git-untracked-face ((,class (:foreground ,red-alt-other))))
-   `(treemacs-help-column-face ((,class (:inherit font-lock-keyword-face :underline t))))
-   `(treemacs-help-title-face ((,class (:inherit font-lock-constant-face))))
+   `(treemacs-help-column-face ((,class (:foreground ,magenta-alt-other :weight ,modus-theme-bold :underline t))))
+   `(treemacs-help-title-face ((,class (:foreground ,blue-alt-other))))
    `(treemacs-on-failure-pulse-face ((,class (:inherit modus-theme-intense-red))))
    `(treemacs-on-success-pulse-face ((,class (:inherit modus-theme-intense-green))))
-   `(treemacs-root-face ((,class (:inherit font-lock-constant-face :height 1.2 :weight bold :underline t))))
-   `(treemacs-root-remote-disconnected-face ((,class (:inherit warning treemacs-root-remote-face))))
-   `(treemacs-root-remote-face ((,class (:inherit font-lock-function-name-face treemacs-root-face))))
+   `(treemacs-root-face ((,class (:foreground ,blue-alt-other :height 1.2 :weight bold :underline t))))
+   `(treemacs-root-remote-disconnected-face ((,class (:inherit treemacs-root-remote-face :foreground ,yellow))))
+   `(treemacs-root-remote-face ((,class (:inherit treemacs-root-face :foreground ,magenta))))
    `(treemacs-root-remote-unreadable-face ((,class (:inherit treemacs-root-unreadable-face))))
    `(treemacs-root-unreadable-face ((,class (:inherit treemacs-root-face :strike-through t))))
    `(treemacs-tags-face ((,class (:foreground ,blue-alt))))

modus-vivendi-theme.el
@@ -392,8 +392,8 @@ between foreground and background is >= 7:1)."
    `(aw-mode-line-face ((,class (:weight bold))))
    ;;;; buttons, links, widgets
    `(button ((,class (:foreground ,blue-alt-other :underline t))))
-   `(link ((,class (:inherit button))))
-   `(link-visited ((,class (:inherit button :foreground ,magenta-alt-other))))
+   `(link ((,class (:foreground ,blue-alt-other :underline t))))
+   `(link-visited ((,class (:foreground ,magenta-alt-other :underline t))))
    `(tooltip ((,class (:inherit modus-theme-subtle-yellow))))
    `(widget-button ((,class (:inherit button))))
    `(widget-button-pressed ((,class (:inherit button :foreground ,magenta))))
@@ -459,9 +459,9 @@ between foreground and background is >= 7:1)."
    `(compilation-mode-line-run ((,class (:foreground ,magenta-active :weight bold))))
    `(compilation-warning ((,class (:foreground ,yellow :weight bold))))
    ;;;; completions
-   `(completions-annotations ((,class (:inherit italic))))
+   `(completions-annotations ((,class (:foreground ,fg-special-cold :slant ,modus-theme-slant))))
    `(completions-common-part ((,class nil)))
-   `(completions-first-difference ((,class (:inherit bold))))
+   `(completions-first-difference ((,class (:weight bold))))
    ;;;; counsel
    `(counsel-active-mode ((,class (:foreground ,magenta-alt-other))))
    `(counsel-application-name ((,class (:foreground ,red-alt-other))))
@@ -754,8 +754,8 @@ between foreground and background is >= 7:1)."
    ;;;; evil-mode
    `(evil-ex-commands ((,class (:foreground ,magenta-alt-other))))
    `(evil-ex-info ((,class (:foreground ,cyan-alt-other))))
-   `(evil-ex-lazy-highlight ((,class (:inherit lazy-highlight))))
-   `(evil-ex-search ((,class (:inherit isearch))))
+   `(evil-ex-lazy-highlight ((,class (:inherit modus-theme-refine-cyan))))
+   `(evil-ex-search ((,class (:inherit modus-theme-intense-green))))
    `(evil-ex-substitute-matches ((,class (:inherit modus-theme-refine-yellow :underline t))))
    `(evil-ex-substitute-replacement ((,class (:inherit modus-theme-intense-green :weight bold))))
    ;;;; evil-goggles
@@ -856,7 +856,7 @@ between foreground and background is >= 7:1)."
       (,class (:foreground ,red :underline t))))
    ;;;; fountain-mode
    `(fountain-character ((,class (:foreground ,magenta-alt-other))))
-   `(fountain-comment ((,class (:inherit font-lock-comment-face))))
+   `(fountain-comment ((,class (:foreground ,fg-alt :slant ,modus-theme-slant))))
    `(fountain-dialog ((,class (:foreground ,blue))))
    `(fountain-metadata-key ((,class (:foreground ,blue-alt-other))))
    `(fountain-metadata-value ((,class (:foreground ,cyan-alt-other))))
@@ -1236,7 +1236,7 @@ between foreground and background is >= 7:1)."
    `(markdown-blockquote-face ((,class (:background ,bg-dim :foreground ,fg-special-warm :slant ,modus-theme-slant))))
    `(markdown-bold-face ((,class (:weight bold))))
    `(markdown-code-face ((,class (:inherit fixed-pitch))))
-   `(markdown-comment-face ((,class (:inherit font-lock-comment-face))))
+   `(markdown-comment-face ((,class (:foreground ,fg-alt :slant ,modus-theme-slant))))
    `(markdown-footnote-marker-face ((,class (:foreground ,cyan-alt :weight bold))))
    `(markdown-footnote-text-face ((,class (:foreground ,fg-main :slant ,modus-theme-slant))))
    `(markdown-gfm-checkbox-face ((,class (:foreground ,cyan-alt-other))))
@@ -1324,7 +1324,7 @@ between foreground and background is >= 7:1)."
    `(mu4e-footer-face ((,class (:foreground ,fg-alt))))
    `(mu4e-forwarded-face ((,class (:foreground ,green-alt-other))))
    `(mu4e-header-face ((,class (:foreground ,fg-main))))
-   `(mu4e-header-highlight-face ((,class (:inherit hl-line))))
+   `(mu4e-header-highlight-face ((,class (:background ,bg-hl-line))))
    `(mu4e-header-key-face ((,class (:foreground ,fg-dim :weight bold))))
    `(mu4e-header-marks-face ((,class (:foreground ,magenta-alt :weight bold))))
    `(mu4e-header-title-face ((,class (:foreground ,fg-special-warm))))
@@ -1334,7 +1334,7 @@ between foreground and background is >= 7:1)."
    `(mu4e-modeline-face ((,class (:foreground ,magenta-active))))
    `(mu4e-moved-face ((,class (:foreground ,blue-refine-fg))))
    `(mu4e-ok-face ((,class (:foreground ,green-intense :weight bold))))
-   `(mu4e-region-code ((,class (:inherit show-paren-match-expression))))
+   `(mu4e-region-code ((,class (:inherit modus-theme-special-calm))))
    `(mu4e-replied-face ((,class (:foreground ,cyan-refine-fg))))
    `(mu4e-special-header-value-face ((,class (:foreground ,magenta :weight bold))))
    `(mu4e-system-face ((,class (:foreground ,fg-alt))))
@@ -1358,10 +1358,10 @@ between foreground and background is >= 7:1)."
    ;;;; neotree
    `(neo-banner-face ((,class (:foreground ,magenta))))
    `(neo-button-face ((,class (:inherit button))))
-   `(neo-dir-link-face ((,class (:inherit dired-directory))))
+   `(neo-dir-link-face ((,class (:foreground ,blue :weight bold))))
    `(neo-expand-btn-face ((,class (:foreground ,cyan))))
    `(neo-file-link-face ((,class (:foreground ,fg-main))))
-   `(neo-header-face ((,class (:inherit dired-header))))
+   `(neo-header-face ((,class (:foreground ,fg-main :weight bold))))
    `(neo-root-dir-face ((,class (:foreground ,cyan-alt :weight bold))))
    `(neo-vc-added-face ((,class (:foreground ,green))))
    `(neo-vc-conflict-face ((,class (:foreground ,red :Weight bold))))
@@ -1452,7 +1452,7 @@ between foreground and background is >= 7:1)."
    `(org-link ((,class (:inherit link))))
    `(org-list-dt ((,class (:foreground ,fg-dim :weight bold))))
    `(org-macro ((,class (:inherit org-latex-and-related))))
-   `(org-meta-line ((,class (:inherit font-lock-comment-face))))
+   `(org-meta-line ((,class (:foreground ,fg-alt :slant ,modus-theme-slant))))
    `(org-mode-line-clock ((,class (:background ,bg-main :foreground ,fg-main))))
    `(org-mode-line-clock-overrun ((,class (:inherit modus-theme-active-red))))
    `(org-priority ((,class (:foreground ,magenta))))
@@ -1601,8 +1601,8 @@ between foreground and background is >= 7:1)."
    `(ruler-mode-pad ((,class (:background ,bg-active :foreground ,fg-inactive))))
    `(ruler-mode-tab-stop ((,class (:inherit ruler-mode-default :foreground ,yellow-active))))
    ;;;; shell scripts
-   `(sh-heredoc ((,class (:inherit font-lock-string-face))))
-   `(sh-quoted-exec ((,class (:inherit font-lock-builtin-face))))
+   `(sh-heredoc ((,class (:foreground ,blue-alt))))
+   `(sh-quoted-exec ((,class (:foreground ,magenta-alt :weight ,modus-theme-bold))))
    ;;;; show-paren-mode
    `(show-paren-match ((,class (:background ,bg-paren-match :foreground ,fg-paren-match :weight bold))))
    `(show-paren-match-expression ((,class (:inherit modus-theme-special-calm))))
@@ -1636,8 +1636,8 @@ between foreground and background is >= 7:1)."
    ;;;; smartparens
    `(sp-pair-overlay-face ((,class (:inherit modus-theme-special-warm))))
    `(sp-show-pair-enclosing ((,class (:inherit modus-theme-special-mild))))
-   `(sp-show-pair-match-face ((,class (:inherit show-paren-match))))
-   `(sp-show-pair-mismatch-face ((,class (:inherit show-paren-mismatch))))
+   `(sp-show-pair-match-face ((,class (:background ,bg-paren-match :foreground ,fg-paren-match :weight bold))))
+   `(sp-show-pair-mismatch-face ((,class (:inherit modus-theme-intense-red))))
    `(sp-wrap-overlay-closing-pair ((,class (:inherit sp-pair-overlay-face :weight bold))))
    `(sp-wrap-overlay-face ((,class (:inherit sp-pair-overlay-face))))
    `(sp-wrap-overlay-opening-pair ((,class (:inherit sp-pair-overlay-face :weight bold))))
@@ -1653,9 +1653,9 @@ between foreground and background is >= 7:1)."
    `(smerge-upper ((,class (:inherit modus-theme-subtle-red))))
    ;;;; speedbar
    `(speedbar-button-face ((,class (:inherit link))))
-   `(speedbar-directory-face ((,class (:inherit dired-directory))))
+   `(speedbar-directory-face ((,class (:foreground ,blue :weight bold))))
    `(speedbar-file-face ((,class (:inherit default))))
-   `(speedbar-highlight-face ((,class (:inherit highlight))))
+   `(speedbar-highlight-face ((,class (:inherit modus-theme-subtle-blue))))
    `(speedbar-selected-face ((,class (:foreground ,cyan :weight bold))))
    `(speedbar-separator-face ((,class (:inherit modus-theme-intense-neutral))))
    `(speedbar-tag-face ((,class (:foreground ,yellow-alt-other))))
@@ -1751,13 +1751,13 @@ between foreground and background is >= 7:1)."
    `(treemacs-git-renamed-face ((,class (:foreground ,cyan-alt-other))))
    `(treemacs-git-unmodified-face ((,class (:foreground ,fg-main))))
    `(treemacs-git-untracked-face ((,class (:foreground ,red-alt-other))))
-   `(treemacs-help-column-face ((,class (:inherit font-lock-keyword-face :underline t))))
-   `(treemacs-help-title-face ((,class (:inherit font-lock-constant-face))))
+   `(treemacs-help-column-face ((,class (:foreground ,magenta-alt-other :weight ,modus-theme-bold :underline t))))
+   `(treemacs-help-title-face ((,class (:foreground ,blue-alt-other))))
    `(treemacs-on-failure-pulse-face ((,class (:inherit modus-theme-intense-red))))
    `(treemacs-on-success-pulse-face ((,class (:inherit modus-theme-intense-green))))
-   `(treemacs-root-face ((,class (:inherit font-lock-constant-face :height 1.2 :weight bold :underline t))))
-   `(treemacs-root-remote-disconnected-face ((,class (:inherit warning treemacs-root-remote-face))))
-   `(treemacs-root-remote-face ((,class (:inherit font-lock-function-name-face treemacs-root-face))))
+   `(treemacs-root-face ((,class (:foreground ,blue-alt-other :height 1.2 :weight bold :underline t))))
+   `(treemacs-root-remote-disconnected-face ((,class (:inherit treemacs-root-remote-face :foreground ,yellow))))
+   `(treemacs-root-remote-face ((,class (:inherit treemacs-root-face :foreground ,magenta))))
    `(treemacs-root-remote-unreadable-face ((,class (:inherit treemacs-root-unreadable-face))))
    `(treemacs-root-unreadable-face ((,class (:inherit treemacs-root-face :strike-through t))))
    `(treemacs-tags-face ((,class (:foreground ,blue-alt))))

commit 32d42b2384277a915e37a720256fc2e4ba79380d

Style the fringe explicitly (related to issue #11)
This also extends a process that started with commit 1ba8d82.  The
idea is to limit `:inherit' to only those cases where a known set of
styles would be used, rather than some unexpected behaviour.

modus-operandi-theme.el
@@ -294,7 +294,7 @@ between foreground and background is >= 7:1)."
    ;;;; absolute essentials
    `(default ((,class (:background ,bg-main :foreground ,fg-main))))
    `(cursor ((,class (:background ,fg-main))))
-   `(fringe ((,class (:inherit default))))
+   `(fringe ((,class (:background ,bg-main :foreground ,fg-main))))
    ;;;; basic and/or ungrouped styles
    `(diary ((,class (:foreground ,yellow))))
    `(error ((,class (:foreground ,red :weight bold))))

modus-vivendi-theme.el
@@ -294,7 +294,7 @@ between foreground and background is >= 7:1)."
    ;;;; absolute essentials
    `(default ((,class (:background ,bg-main :foreground ,fg-main))))
    `(cursor ((,class (:background ,fg-main))))
-   `(fringe ((,class (:inherit default))))
+   `(fringe ((,class (:background ,bg-main :foreground ,fg-main))))
    ;;;; basic and/or ungrouped styles
    `(diary ((,class (:foreground ,yellow))))
    `(error ((,class (:foreground ,red :weight bold))))