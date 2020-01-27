c3f25414acff319307094b10ffd771aebc2455f2
Protesilaos Stavrou <info@protesilaos.com>
2020-01-27 11:02:51 +0200

    Update `Commentary' section with better docs

--- modus-vivendi-theme.el
+++ modus-vivendi-theme.el
@@ -33,14 +33,161 @@
 ;; Consortium's Working Group on Accessibility (WCAG AAA standard).
 ;;
 ;; 2. Offer as close to full face coverage as possible.  The list is
-;; already quite long, with more additions to follow.
+;; already quite long (see further below), with more additions to follow
+;; as part of the ongoing development process.
 ;;
-;; Note though, that it is difficult to create theme styles without
-;; actually using the packages/interfaces that require them.  The output
-;; of `list-faces-display' offers all we need to get started.  As such,
-;; the development of this theme will be incremental, gradually
-;; incorporating the customisations for packages I use or have been
-;; exposed to.
+;; The theme provides the following customisation options, all of which
+;; are disabled by default:
+;;
+;;     modus-vivendi-theme-slanted-constructs
+;;     modus-vivendi-theme-bold-constructs
+;;     modus-vivendi-theme-proportional-fonts
+;;     modus-vivendi-theme-scale-headings
+;;
+;; The default scale is as follows (it can be customised as well):
+;;
+;;     modus-vivendi-theme-scale-1 1.05
+;;     modus-vivendi-theme-scale-2 1.1
+;;     modus-vivendi-theme-scale-3 1.15
+;;     modus-vivendi-theme-scale-4 1.2
+;;
+;; What follows is the list of explicitly supported packages or face
+;; groups (there are implicitly supported packages as well, which
+;; inherit from font-lock or some basic group).  You are encouraged to
+;; notify me of any missing package or change you would like to see.
+;;
+;;     all-the-icons
+;;     annotate
+;;     anzu
+;;     apt-sources-list
+;;     artbollocks-mode
+;;     auto-dim-other-buffers
+;;     avy
+;;     ace-window
+;;     calendar
+;;     calfw
+;;     column-enforce-mode
+;;     company-mode
+;;     company-posframe
+;;     compilation-mode
+;;     completions
+;;     counsel
+;;     counsel-css
+;;     counsel-notmuch
+;;     counsel-org-capture-string
+;;     cov
+;;     custom (M-x customize)
+;;     dashboard (emacs-dashboard)
+;;     deadgrep
+;;     define-word
+;;     disk-usage
+;;     diff-hl
+;;     diff-mode
+;;     dired
+;;     dired-async
+;;     dired-git-info
+;;     dired-narrow
+;;     dired-subtree
+;;     diredfl
+;;     doom-modeline
+;;     easy-jekyll
+;;     easy-kill
+;;     ediff
+;;     eldoc-box
+;;     elfeed
+;;     emms
+;;     epa
+;;     equake
+;;     erc
+;;     ert
+;;     eshell
+;;     evil (evil-mode)
+;;     evil-goggles
+;;     evil-visual-mark-mode
+;;     eww
+;;     eyebrowse
+;;     fancy-dabbrev
+;;     focus
+;;     font-lock (generic syntax highlighting)
+;;     flycheck
+;;     flycheck-indicator
+;;     flycheck-posframe
+;;     flymake
+;;     flyspell
+;;     fountain (fountain-mode)
+;;     geiser
+;;     git
+;;     git-gutter (and variants)
+;;     git-lens
+;;     git-timemachine
+;;     gnus
+;;     helm
+;;     hydra
+;;     highlight-blocks
+;;     hl-line-mode
+;;     hl-fill-column
+;;     ido-mode
+;;     iedit
+;;     info
+;;     info-colors
+;;     isearch, occur, etc.
+;;     ivy
+;;     ivy-posframe
+;;     keycast
+;;     line numbers (`display-line-numbers-mode' and global variant)
+;;     lsp-mode
+;;     lsp-ui
+;;     magit
+;;     markdown-mode
+;;     mentor
+;;     messages
+;;     modeline
+;;     mood-line
+;;     mu4e
+;;     mu4e-conversation
+;;     neotree
+;;     org
+;;     org-journal
+;;     org-noter
+;;     org-recur
+;;     outline-mode
+;;     package (M-x list-packages)
+;;     paren-face
+;;     pass
+;;     persp-mode
+;;     perspective
+;;     powerline
+;;     powerline-evil
+;;     proced
+;;     prodigy
+;;     rainbow-blocks
+;;     rainbow-delimiters
+;;     regexp-builder (also known as `re-builder')
+;;     ruler-mode
+;;     shell-script-mode
+;;     show-paren-mode
+;;     smart-mode-line
+;;     smartparens
+;;     smerge
+;;     speedbar
+;;     suggest
+;;     swiper
+;;     sx
+;;     telephone-line
+;;     term
+;;     transient (pop-up windows like Magit's)
+;;     treemacs
+;;     undo-tree
+;;     vc (built-in mode line status for version control)
+;;     visual-regexp
+;;     wgrep
+;;     which-function-mode
+;;     which-key
+;;     whitespace-mode
+;;     writegood-mode
+;;     xah-elisp-mode
+;;     xterm-color (and ansi-colors)
+;;     ztree
 
 ;;; Code:
 
