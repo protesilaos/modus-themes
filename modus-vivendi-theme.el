5c221660140092ddd3aadf4d62140e44c034a058
Protesilaos Stavrou <info@protesilaos.com>
2020-03-26 20:37:53 +0200

    Add support for EBDB

--- modus-vivendi-theme.el
+++ modus-vivendi-theme.el
@@ -109,6 +109,7 @@
 ;;     dynamic-ruler
 ;;     easy-jekyll
 ;;     easy-kill
+;;     ebdb
 ;;     ediff
 ;;     eldoc-box
 ;;     elfeed
@@ -1115,6 +1116,21 @@ between foreground and background is >= 7:1)."
    ;;;; easy-kill
    `(easy-kill-origin ((,class (:inherit modus-theme-subtle-red))))
    `(easy-kill-selection ((,class (:inherit modus-theme-subtle-yellow))))
+   ;;;; ebdb
+   `(ebdb-address-default ((,class (:foreground ,fg-main))))
+   `(ebdb-db-char ((,class (:foreground ,fg-special-cold))))
+   `(ebdb-defunct ((,class (:foreground ,fg-alt))))
+   `(ebdb-field-hidden ((,class (:foreground ,magenta))))
+   `(ebdb-field-url ((,class (:foreground ,blue))))
+   `(ebdb-label ((,class (:foreground ,cyan-alt-other))))
+   `(ebdb-mail-default ((,class (:foreground ,fg-main))))
+   `(ebdb-mail-primary ((,class (:foreground ,blue-alt))))
+   `(ebdb-marked ((,class (:background ,cyan-intense-bg))))
+   `(ebdb-organization-name ((,class (:foreground ,fg-special-calm))))
+   `(ebdb-person-name ((,class (:foreground ,magenta-alt-other))))
+   `(ebdb-phone-default ((,class (:foreground ,fg-special-warm))))
+   `(ebdb-role-defunct ((,class (:foreground ,fg-alt))))
+   `(eieio-custom-slot-tag-face ((,class (:foreground ,red-alt))))
    ;;;; ediff
    `(ediff-current-diff-A ((,class (:inherit modus-theme-special-warm))))
    `(ediff-current-diff-Ancestor ((,class (:background ,bg-alt :foreground ,fg-main))))
