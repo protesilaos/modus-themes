9ddae384b84e2e6ab44055a70435b506d7a2565c
Protesilaos Stavrou <info@protesilaos.com>
2020-11-11 13:15:29 +0200

    Remove unused macro for simplicity

--- modus-themes-faces.el
+++ modus-themes-faces.el
@@ -6,17 +6,6 @@
 ;; -
 ;;; Code:
 
-;; (defmacro modus-themes-faces ()
-;;   "Return a backquote which defines a list of face specs.
-;; It expects to be evaluated in a scope in which the various color
-;; names to which it refers are bound."
-;;   (quote
-;;    (mapcar
-;;     (lambda (entry)
-;;       (list (car entry) `((,class ,@(cdr entry)))))
-;;     `(
-;;       (default :background ,bg-main :foreground ,fg-main)))))
-
 (defun modus-themes-faces ()
   "Face specs for use with `modus-themes-core-with-colors'."
   (quote
