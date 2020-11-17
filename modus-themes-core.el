7d1fb78c6a29a9ea23dfb9213818839c3b6199db
Protesilaos Stavrou <info@protesilaos.com>
2020-11-17 17:11:28 +0200

    Add comment headers for core and main code files
    Discussion: https://gitlab.com/protesilaos/modus-themes/-/issues/119

--- modus-themes-core.el
+++ modus-themes-core.el
@@ -1,10 +1,46 @@
-;;; modus-themes-core.el --- Testing -*- lexical-binding:t -*-
-;; URL: https://www.protesilaos.com
-;; Version: 0.0.0
+;;; modus-themes-core.el --- Core code of the Modus themes -*- lexical-binding:t -*-
+
+;; Copyright (C) 2019-2020 Free Software Foundation, Inc.
+
+;; Author: Protesilaos Stavrou <info@protesilaos.com>
+;; URL: https://gitlab.com/protesilaos/modus-themes
+;; Version: 1.0.0
 ;; Package-Requires: ((emacs "26.1"))
+;; Keywords: faces, theme, accessibility
+
+;; This file is part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
-;; -
+;;
+;; The Modus themes conform with the highest standard for color-contrast
+;; accessibility between background and foreground values (WCAG AAA).
+;; This file contains all customisation options, helper functions,
+;; interactive commands, and face specifications.
+;;
+;; For a complete view of the project, also refer to the following files
+;; (should be distributed in the same repository/directory as the
+;; current item):
+;;
+;; - modus-themes.el            (Main code shared between the themes)
+;; - modus-operandi-theme.el    (Light theme)
+;; - modus-vivendi-theme.el     (Dark theme)
+
 ;;; Code:
+
+
 
 (defvar modus-themes-operandi-colors)
 (defvar modus-themes-vivendi-colors)
