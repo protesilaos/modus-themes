3f53316f7bb969b1b49f733399a24b9b91a30932
Protesilaos Stavrou <info@protesilaos.com>
2020-11-19 22:42:53 +0200

    Tweak call to load path
    * modus-operandi-theme.el (load-file-name): Make sure
    custom-theme-load-path is bound.
    
    * modus-vivendi-theme.el (load-file-name): Same.

--- modus-operandi-theme.el
+++ modus-operandi-theme.el
@@ -59,7 +59,7 @@
  'modus-operandi)
 
 ;;;###autoload
-(when load-file-name
+(when (and (boundp 'custom-theme-load-path) load-file-name)
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
 
