ab0f558397bb674342176c37c22190c0c1e54cc1
Protesilaos Stavrou <info@protesilaos.com>
2020-12-06 13:08:37 +0200

    Update to version 1.0.2
    Minor tweaks to the manual to ensure that the right instructions are
    provided for those who install the themes manually or will be using the
    version that is shipped with Emacs.

--- modus-operandi-theme.el
+++ modus-operandi-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: faces, theme, accessibility
 
