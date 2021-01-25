46bd9afbdf111a0df39ab00f0b71fa9fddffec4e
Protesilaos Stavrou <info@protesilaos.com>
2021-01-25 07:52:16 +0200

    Restore mistakenly removed section from the manual

--- modus-operandi-theme.el
+++ modus-operandi-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
-;; Version: 1.1.0
+;; Version: 1.1.1
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: faces, theme, accessibility
 
