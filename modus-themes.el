3b2ee734da3b47972ebc2a62c344224f34b5636b
Protesilaos Stavrou <info@protesilaos.com>
2021-03-02 11:13:17 +0200

    Simplify annulment of dired-subtree backgrounds

--- modus-themes.el
+++ modus-themes.el
@@ -3696,15 +3696,16 @@ by virtue of calling either of `modus-themes-load-operandi' and
 ;;;;; dired-narrow
     `(dired-narrow-blink ((,class :inherit (modus-theme-subtle-cyan bold))))
 ;;;;; dired-subtree
-    ;; remove background from dired-subtree, else it breaks
+    ;; remove backgrounds from dired-subtree faces, else they break
     ;; dired-{flagged,marked} and any other face that sets a background
-    ;; such as hl-line
-    `(dired-subtree-depth-1-face ((,class :background nil)))
-    `(dired-subtree-depth-2-face ((,class :background nil)))
-    `(dired-subtree-depth-3-face ((,class :background nil)))
-    `(dired-subtree-depth-4-face ((,class :background nil)))
-    `(dired-subtree-depth-5-face ((,class :background nil)))
-    `(dired-subtree-depth-6-face ((,class :background nil)))
+    ;; such as hl-line.  Also, denoting depth by varying shades of gray
+    ;; is not good for accessibility.
+    `(dired-subtree-depth-1-face (()))
+    `(dired-subtree-depth-2-face (()))
+    `(dired-subtree-depth-3-face (()))
+    `(dired-subtree-depth-4-face (()))
+    `(dired-subtree-depth-5-face (()))
+    `(dired-subtree-depth-6-face (()))
 ;;;;; diredc
     `(diredc-face-chmod-font-lock-dir ((,class :foreground ,blue-alt)))
     `(diredc-face-chmod-font-lock-exec ((,class :foreground ,magenta)))
