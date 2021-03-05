ece5f3b92ed070989492cf40638a1954af0f2384
Protesilaos Stavrou <info@protesilaos.com>
2021-03-05 14:03:04 +0200

    Update to version 1.2.2

--- modus-themes.el
+++ modus-themes.el
@@ -4,7 +4,7 @@
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
-;; Version: 1.2.0
+;; Version: 1.2.2
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: faces, theme, accessibility
 
