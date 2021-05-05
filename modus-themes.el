14743392d13ee0e1dc42f840cec2e4ce832e5218
Protesilaos Stavrou <info@protesilaos.com>
2021-05-05 09:07:49 +0300

    Add support for new bookmark-face (emacs28)
    This face can be disabled with the following:
    
        (setq bookmark-fontify nil)
    
    Thanks to Mark Barton for reporting the presence of this new face and
    for providing valuable feedback in issue 189:
    <https://gitlab.com/protesilaos/modus-themes/-/issues/189>.

--- modus-themes.el
+++ modus-themes.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
 ;; Version: 1.3.2
-;; Last-Modified: <2021-05-04 19:51:18 +0300>
+;; Last-Modified: <2021-05-05 09:04:22 +0300>
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: faces, theme, accessibility
 
@@ -3449,6 +3449,8 @@ by virtue of calling either of `modus-themes-load-operandi' and
     `(boon-modeline-ins ((,class :inherit modus-themes-active-red)))
     `(boon-modeline-off ((,class :inherit modus-themes-active-yellow)))
     `(boon-modeline-spc ((,class :inherit modus-themes-active-green)))
+;;;;; bookmark
+    `(bookmark-face ((,class :inherit modus-themes-special-warm :extend t)))
 ;;;;; breakpoint (built-in gdb-mi.el)
     `(breakpoint-disabled ((,class :inherit shadow)))
     `(breakpoint-enabled ((,class :inherit bold :foreground ,red)))
