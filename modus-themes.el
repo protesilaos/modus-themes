protesilaos/modus-themes

commit 87b157cd7158821f441e33a5b829818cc9563674

Tweak mode-line 'padded' height (see 0e7c5c6a16)
This is based on further feedback by Manuel Uberti in issue 228:
<https://gitlab.com/protesilaos/modus-themes/-/issues/228>.

modus-themes.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
 ;; Version: 1.5.0
-;; Last-Modified: <2021-07-28 10:40:56 +0300>
+;; Last-Modified: <2021-07-28 10:45:11 +0300>
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: faces, theme, accessibility
 
@@ -3451,9 +3451,9 @@ property."
                             :style (when alt-style 'released-button)))
                      ((and (memq 'accented modus-themes-mode-line)
                            (memq 'padded modus-themes-mode-line))
-                      (list :line-width 4 :color bg-accent))
+                      (list :line-width 6 :color bg-accent))
                      ((memq 'padded modus-themes-mode-line)
-                      (list :line-width 4 :color bg))
+                      (list :line-width 6 :color bg))
                      ((memq '3d modus-themes-mode-line)
                       (list :line-width (or border-width 1)
                             :color
@@ -3468,7 +3468,7 @@ property."
                      ((memq 'borderless modus-themes-mode-line)
                       bg)
                      ((memq 'padded modus-themes-mode-line)
-                      (list :line-width 4 :color bg))
+                      (list :line-width 6 :color bg))
                      (border)))
           (line (cond ((not (or (memq 'moody modus-themes-mode-line)
                                 (memq 'padded modus-themes-mode-line)))

commit e486267065c7182138790b05b89f9cbd2354a214

Fix stylistic regression from 87b157cd71
It would make (setq modus-themes-mode-line '(accented)) look the same as
if it had the 'borderless' property as well.

modus-themes.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
 ;; Version: 1.5.0
-;; Last-Modified: <2021-07-28 15:04:56 +0300>
+;; Last-Modified: <2021-07-28 15:25:10 +0300>
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: faces, theme, accessibility
 
@@ -3460,8 +3460,6 @@ property."
                                   ((memq 'borderless modus-themes-mode-line) bg)
                                   (border-3d))
                             :style (when alt-style 'released-button)))
-                     ((memq 'accented modus-themes-mode-line)
-                      bg-accent)
                      ((memq 'borderless modus-themes-mode-line)
                       bg)
                      ((memq 'padded modus-themes-mode-line)