8c0a9af554e2b0054c3bb8ecf75ea94ebfa88e60
Protesilaos Stavrou <info@protesilaos.com>
2021-09-07 20:14:56 +0300

    Ensure bookmark-menu-bookmark inherits from bold
    By default it hardcodes the bold weight, whereas we instruct it to
    inherit the 'bold' face.  So a user can change the ':weight' of that
    face to whatever they want, like semibold, extrabold, etc.  We do this
    throughout the themes.
    
    Check the manual for more on the matter:
    
        (info "(modus-themes) Configure bold and italic faces (DIY)")

--- modus-themes.el
+++ modus-themes.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
 ;; Version: 1.5.0
-;; Last-Modified: <2021-09-06 14:01:05 +0300>
+;; Last-Modified: <2021-09-07 20:12:02 +0300>
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: faces, theme, accessibility
 
@@ -4395,6 +4395,7 @@ by virtue of calling either of `modus-themes-load-operandi' and
     `(boon-modeline-spc ((,class :inherit modus-themes-active-green)))
 ;;;;; bookmark
     `(bookmark-face ((,class :background ,bg-hl-alt :extend t)))
+    `(bookmark-menu-bookmark ((,class :inherit bold)))
 ;;;;; breakpoint (built-in gdb-mi.el)
     `(breakpoint-disabled ((,class :inherit shadow)))
     `(breakpoint-enabled ((,class :inherit bold :foreground ,red)))
