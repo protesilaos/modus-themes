54d5b4fc61ec85002873ddd6fe9d68296074744d
Protesilaos Stavrou <info@protesilaos.com>
2021-12-29 09:56:03 +0200

    Use more appropriate style for info-menu-header

--- modus-themes.el
+++ modus-themes.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
 ;; Version: 2.0.0
-;; Last-Modified: <2021-12-28 08:02:43 +0200>
+;; Last-Modified: <2021-12-29 09:55:30 +0200>
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: faces, theme, accessibility
 
@@ -5754,7 +5754,7 @@ by virtue of calling either of `modus-themes-load-operandi' and
     `(info-header-node ((,class :inherit (shadow bold))))
     `(info-header-xref ((,class :foreground ,blue-active)))
     `(info-index-match ((,class :inherit match)))
-    `(info-menu-header ((,class :inherit modus-themes-heading-3)))
+    `(info-menu-header ((,class :inherit modus-themes-pseudo-header)))
     `(info-menu-star ((,class :foreground ,red)))
     `(info-node ((,class :inherit bold)))
     `(info-title-1 ((,class :inherit modus-themes-heading-1)))
