19309151a04b666df0a536a700f947362e1e10e5
Protesilaos Stavrou <info@protesilaos.com>
2022-01-21 22:23:38 +0200

    Make echo area current arg more prominent
    In commit be886d3 I wrote:
    
        The red colour feels too aggressive.  I tried it for two full days and
        still cannot get used to it.
    
        This revises commits aa62a2a, fe911a4.  It relates to issue 275:
        <https://gitlab.com/protesilaos/modus-themes/-/issues/275>.
    
    Though the latest feedback I got from Rudolf Adamkovič includes
    information I had not considered before: that one may check the
    indicator using their peripheral vision.  Whereas I have the habit of
    actually focusing on the echo area.
    
    Couched in those terms, the red hue is an appropriate choice: it may
    still be too prominent for my use-case, but even so it remains usable.
    We thus have one case where the red is essential and another where it is
    not impeding the given functionality.
    
    If someone thinks the red is intolerable, then we will have to introduce
    a defcustom.
    
    At any rate, I don't want to have a background there by default because
    it is easy to mistake it for a mouse highlight or something that has to
    do with minibuffer completion.  An underline is problematic as well: it
    is not visible in fullscreen view and it does not work as well when
    using M-: (the eval-expression command) where the Eldoc indicators move
    to the mode line.

--- modus-themes.el
+++ modus-themes.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
 ;; Version: 2.0.0
-;; Last-Modified: <2022-01-21 20:31:23 +0200>
+;; Last-Modified: <2022-01-21 22:12:20 +0200>
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: faces, theme, accessibility
 
@@ -5078,7 +5078,7 @@ by virtue of calling either of `modus-themes-load-operandi' and
     `(el-search-occur-match ((,class :inherit modus-themes-special-calm)))
 ;;;;; eldoc
     ;; NOTE: see https://github.com/purcell/package-lint/issues/187
-    (list 'eldoc-highlight-function-argument `((,class :inherit bold :foreground ,cyan-alt-other)))
+    (list 'eldoc-highlight-function-argument `((,class :inherit bold :foreground ,red-alt-other)))
 ;;;;; eldoc-box
     `(eldoc-box-body ((,class :background ,bg-alt :foreground ,fg-main)))
     `(eldoc-box-border ((,class :background ,fg-alt)))
@@ -5441,8 +5441,8 @@ by virtue of calling either of `modus-themes-load-operandi' and
     `(fountain-synopsis ((,class :foreground ,cyan-alt)))
     `(fountain-trans ((,class :foreground ,yellow-alt-other)))
 ;;;;; geiser
-    `(geiser-font-lock-autodoc-current-arg ((,class :inherit bold :foreground ,cyan-alt-other)))
-    `(geiser-font-lock-autodoc-identifier ((,class :foreground ,magenta-alt-other)))
+    `(geiser-font-lock-autodoc-current-arg ((,class :inherit bold :foreground ,red-alt-other)))
+    `(geiser-font-lock-autodoc-identifier ((,class :foreground ,cyan)))
     `(geiser-font-lock-doc-button ((,class :inherit button :foreground ,fg-docstring)))
     `(geiser-font-lock-doc-link ((,class :inherit button)))
     `(geiser-font-lock-error-link ((,class :inherit button :foreground ,red)))
