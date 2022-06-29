7241ea0a4160a34167720964190681f2d70139ed
Protesilaos Stavrou <info@protesilaos.com>
2022-06-29 08:46:58 +0300

    Restore link to mailing list archive

--- modus-operandi-theme.el
+++ modus-operandi-theme.el
@@ -5,6 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Modus-Themes Development <~protesilaos/modus-themes@lists.sr.ht>
 ;; URL: https://git.sr.ht/~protesilaos/modus-themes
+;; Mailing-List: https://lists.sr.ht/~protesilaos/modus-themes
 ;; Version: 2.4.1
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: faces, theme, accessibility
