abe2047067fe4be527a1dc0ddca27035e19980ca
Protesilaos Stavrou <info@protesilaos.com>
2022-07-14 14:15:20 +0300

    Refine colour of Compose key in PGTK builds
    The underline seems to be hard-coded, so I am removing it from the face
    here.

--- modus-themes.el
+++ modus-themes.el
@@ -4384,7 +4384,7 @@ by virtue of calling either of `modus-themes-load-operandi' and
     `(mm-command-output ((,class :foreground ,red-alt-other)))
     `(mm-uu-extract ((,class :background ,bg-dim :foreground ,fg-special-mild)))
     `(next-error ((,class :inherit modus-themes-subtle-red :extend t)))
-    `(pgtk-im-0 ((,class :inherit modus-themes-fringe-blue :underline t)))
+    `(pgtk-im-0 ((,class :inherit modus-themes-refine-cyan)))
     `(rectangle-preview ((,class :background ,bg-special-faint-warm :foreground ,fg-special-warm)))
     `(region ((,class ,@(modus-themes--region bg-region fg-main
                                               bg-hl-alt-intense bg-region-accent
