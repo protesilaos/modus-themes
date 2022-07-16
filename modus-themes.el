a135569c37572487d8b04f74fe73f6422d223059
Protesilaos Stavrou <info@protesilaos.com>
2022-07-16 17:49:46 +0300

    Add more colour to date background in Org agenda
    Makes it easier to discern where the highlight is, while it remains
    close to the spirit of the previous design.  The idea is to not add too
    much saturation here, because the buffer is already "busy" with lots of
    highlights.
    
    Thanks to Daniel Mendler for the feedback on the mailing list:
    <https://lists.sr.ht/~protesilaos/modus-themes/%3C3d8b1096-a7db-1e08-fefe-d39bed4a7ea3@daniel-mendler.de%3E>.

--- modus-themes.el
+++ modus-themes.el
@@ -6490,12 +6490,12 @@ by virtue of calling either of `modus-themes-load-operandi' and
     `(org-agenda-date ((,class ,@(modus-themes--agenda-date cyan fg-main))))
     `(org-agenda-date-today ((,class ,@(modus-themes--agenda-date cyan fg-main
                                                                   nil nil
-                                                                  bg-inactive t t))))
+                                                                  bg-special-cold t t))))
     `(org-agenda-date-weekend ((,class ,@(modus-themes--agenda-date cyan-alt-other-faint fg-alt
                                                                     cyan fg-main))))
     `(org-agenda-date-weekend-today ((,class ,@(modus-themes--agenda-date cyan-alt-other-faint fg-alt
                                                                           cyan fg-main
-                                                                          bg-inactive t t))))
+                                                                          bg-special-cold t t))))
     `(org-agenda-diary ((,class :inherit org-agenda-calendar-sexp)))
     `(org-agenda-dimmed-todo-face ((,class :inherit shadow)))
     `(org-agenda-done ((,class :inherit modus-themes-grue-nuanced)))
