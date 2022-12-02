2bc25222663f188505b02f629b829862b5a20c81
Protesilaos Stavrou <info@protesilaos.com>
2022-12-02 10:03:31 +0200

    modus-vivendi: tweak accent-2 mappings

--- modus-vivendi-theme.el
+++ modus-vivendi-theme.el
@@ -210,11 +210,11 @@ which corresponds to a minimum contrast in relative luminance of
 
       (accent-0 blue-cooler)
       (accent-1 magenta-warmer)
-      (accent-2 green-cooler)
+      (accent-2 cyan-cooler)
       (accent-3 yellow)
       (bg-accent-0 bg-blue-subtle)
       (bg-accent-1 bg-magenta-subtle)
-      (bg-accent-2 bg-green-subtle)
+      (bg-accent-2 bg-cyan-subtle)
       (bg-accent-3 bg-yellow-subtle)
 
       (mail-0 blue-warmer)
