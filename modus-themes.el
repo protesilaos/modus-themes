32d6360a5a836c4c17bf47dc5803a9abeb6bea83
Protesilaos Stavrou <info@protesilaos.com>
2022-12-03 06:51:59 +0200

    Refine bongo faces

--- modus-themes.el
+++ modus-themes.el
@@ -1839,7 +1839,7 @@ C1 and C2 are color values written in hexadecimal RGB."
     `(binder-sidebar-tags ((,c :foreground ,variable)))
 ;;;;; bongo
     `(bongo-album-title (( )))
-    `(bongo-artist ((,c :foreground ,variable)))
+    `(bongo-artist ((,c :foreground ,accent-0)))
     `(bongo-currently-playing-track ((,c :inherit bold)))
     `(bongo-elapsed-track-part ((,c :background ,bg-inactive :underline t)))
     `(bongo-filled-seek-bar ((,c :background ,bg-hover)))
@@ -1847,7 +1847,7 @@ C1 and C2 are color values written in hexadecimal RGB."
     `(bongo-marked-track-line ((,c :background ,bg-dim)))
     `(bongo-played-track ((,c :inherit shadow :strike-through t)))
     `(bongo-track-length ((,c :inherit shadow)))
-    `(bongo-track-title ((,c :foreground ,builtin)))
+    `(bongo-track-title ((,c :foreground ,accent-1)))
     `(bongo-unfilled-seek-bar ((,c :background ,bg-dim)))
 ;;;;; boon
     `(boon-modeline-cmd ((,c :inherit modus-themes-intense-blue)))
