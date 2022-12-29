protesilaos/modus-themes

commit 7c74495bf76359b6895891ff9dcc6529ab856a74

Give appropriate name to intense backgrounds

modus-themes.el
@@ -1484,12 +1484,12 @@ is a less intense variant of BG."
     `(modus-themes-subtle-magenta ((,c :background ,bg-magenta-subtle :foreground ,fg-main)))
     `(modus-themes-subtle-cyan ((,c :background ,bg-cyan-subtle :foreground ,fg-main)))
 ;;;;; intense colored backgrounds
-    `(modus-themes-intense-red ((,c :background ,bg-red :foreground ,fg-main)))
-    `(modus-themes-intense-green ((,c :background ,bg-green :foreground ,fg-main)))
-    `(modus-themes-intense-yellow ((,c :background ,bg-yellow :foreground ,fg-main)))
-    `(modus-themes-intense-blue ((,c :background ,bg-blue :foreground ,fg-main)))
-    `(modus-themes-intense-magenta ((,c :background ,bg-magenta :foreground ,fg-main)))
-    `(modus-themes-intense-cyan ((,c :background ,bg-cyan :foreground ,fg-main)))
+    `(modus-themes-intense-red ((,c :background ,bg-red-intense :foreground ,fg-main)))
+    `(modus-themes-intense-green ((,c :background ,bg-green-intense :foreground ,fg-main)))
+    `(modus-themes-intense-yellow ((,c :background ,bg-yellow-intense :foreground ,fg-main)))
+    `(modus-themes-intense-blue ((,c :background ,bg-blue-intense :foreground ,fg-main)))
+    `(modus-themes-intense-magenta ((,c :background ,bg-magenta-intense :foreground ,fg-main)))
+    `(modus-themes-intense-cyan ((,c :background ,bg-cyan-intense :foreground ,fg-main)))
 ;;;;; mark indicators
     ;; color combinations intended for Dired, Ibuffer, or equivalent
     `(modus-themes-mark-alt ((,c :inherit bold :background ,bg-yellow-subtle :foreground ,yellow)))
@@ -1516,8 +1516,8 @@ is a less intense variant of BG."
     `(modus-themes-prose-macro ((,c :foreground ,prose-macro)))
     `(modus-themes-prose-verbatim ((,c :foreground ,prose-verbatim)))
 ;;;;; search
-    `(modus-themes-search-current ((,c :background ,bg-yellow :foreground ,fg-main)))
-    `(modus-themes-search-lazy ((,c :background ,bg-cyan :foreground ,fg-main)))
+    `(modus-themes-search-current ((,c :background ,bg-yellow-intense :foreground ,fg-main)))
+    `(modus-themes-search-lazy ((,c :background ,bg-cyan-intense :foreground ,fg-main)))
 ;;;;; completion frameworks
     `(modus-themes-completion-match-0 ((,c ,@(modus-themes--completion-match fg-accent-0 bg-accent-0))))
     `(modus-themes-completion-match-1 ((,c ,@(modus-themes--completion-match fg-accent-1 bg-accent-1))))
@@ -1582,7 +1582,7 @@ is a less intense variant of BG."
     `(separator-line ((,c :underline ,bg-active)))
     `(shadow ((,c :foreground ,fg-dim)))
     `(success ((,c :inherit bold :foreground ,info)))
-    `(trailing-whitespace ((,c :background ,bg-red)))
+    `(trailing-whitespace ((,c :background ,bg-red-intense)))
     `(warning ((,c :inherit bold :foreground ,warning)))
 ;;;;; buttons, links, widgets
     `(button ((,c :background ,bg-link :foreground ,fg-link :underline ,underline-link)))
@@ -1843,7 +1843,7 @@ is a less intense variant of BG."
     `(company-echo-common ((,c :inherit modus-themes-completion-match-0)))
     `(company-preview ((,c :background ,bg-dim :foreground ,fg-dim)))
     `(company-preview-common ((,c :inherit company-echo-common)))
-    `(company-preview-search ((,c :background ,bg-yellow)))
+    `(company-preview-search ((,c :background ,bg-yellow-intense)))
     `(company-scrollbar-bg ((,c :background ,bg-active)))
     `(company-scrollbar-fg ((,c :background ,fg-main)))
     `(company-template-field ((,c :background ,bg-active)))
@@ -2835,7 +2835,7 @@ is a less intense variant of BG."
     `(message-separator ((,c :background ,bg-active)))
 ;;;;; minimap
     `(minimap-active-region-background ((,c :background ,bg-active)))
-    `(minimap-current-line-face ((,c :background ,bg-cyan :foreground ,fg-main)))
+    `(minimap-current-line-face ((,c :background ,bg-cyan-intense :foreground ,fg-main)))
 ;;;;; mode-line
     `(mode-line ((,c :inherit modus-themes-ui-variable-pitch
                      :box ,border-mode-line-active
@@ -3146,7 +3146,7 @@ is a less intense variant of BG."
     `(pass-mode-entry-face ((,c :background ,bg-main :foreground ,fg-main)))
     `(pass-mode-header-face ((,c :inherit shadow)))
 ;;;;; pdf-tools
-    `(pdf-links-read-link ((,c :background ,fg-main :foreground ,bg-magenta :inherit bold))) ; Foreground is background and vice versa
+    `(pdf-links-read-link ((,c :background ,fg-main :foreground ,bg-magenta-intense :inherit bold))) ; Foreground is background and vice versa
     `(pdf-occur-document-face ((,c :inherit shadow)))
     `(pdf-occur-page-face ((,c :inherit shadow)))
 ;;;;; persp-mode
@@ -3192,7 +3192,7 @@ is a less intense variant of BG."
     `(prodigy-red-face ((,c :inherit error)))
     `(prodigy-yellow-face ((,c :inherit warning)))
 ;;;;; pulse
-    `(pulse-highlight-start-face ((,c :background ,bg-blue :extend t)))
+    `(pulse-highlight-start-face ((,c :background ,bg-blue-intense :extend t)))
 ;;;;; pyim
     `(pyim-page ((,c :background ,bg-active)))
     `(pyim-page-selection ((,c :inherit bold :background ,bg-active :foreground ,info)))
@@ -3584,11 +3584,11 @@ is a less intense variant of BG."
     `(vimish-fold-mouse-face ((,c :inherit modus-themes-intense-blue)))
     `(vimish-fold-overlay ((,c :background ,bg-inactive)))
 ;;;;; visible-mark
-    `(visible-mark-active ((,c :background ,bg-blue)))
-    `(visible-mark-face1 ((,c :background ,bg-cyan)))
-    `(visible-mark-face2 ((,c :background ,bg-yellow)))
-    `(visible-mark-forward-face1 ((,c :background ,bg-magenta)))
-    `(visible-mark-forward-face2 ((,c :background ,bg-green)))
+    `(visible-mark-active ((,c :background ,bg-blue-intense)))
+    `(visible-mark-face1 ((,c :background ,bg-cyan-intense)))
+    `(visible-mark-face2 ((,c :background ,bg-yellow-intense)))
+    `(visible-mark-forward-face1 ((,c :background ,bg-magenta-intense)))
+    `(visible-mark-forward-face2 ((,c :background ,bg-green-intense)))
 ;;;;; visual-regexp
     `(vr/group-0 ((,c :inherit modus-themes-intense-blue)))
     `(vr/group-1 ((,c :inherit modus-themes-intense-magenta)))

modus-operandi-deuteranopia-theme.el
@@ -110,12 +110,12 @@ standard).")
 
 ;;; Common accent backgrounds
 
-      (bg-red      "#ff8f88")
-      (bg-green    "#8adf80")
-      (bg-yellow   "#f3d000")
-      (bg-blue     "#bfc9ff")
-      (bg-magenta  "#dfa0f0")
-      (bg-cyan     "#a4d5f9")
+      (bg-red-intense     "#ff8f88")
+      (bg-green-intense   "#8adf80")
+      (bg-yellow-intense  "#f3d000")
+      (bg-blue-intense    "#bfc9ff")
+      (bg-magenta-intense "#dfa0f0")
+      (bg-cyan-intense    "#a4d5f9")
 
       (bg-red-subtle      "#ffcfbf")
       (bg-green-subtle    "#b3fabf")

modus-operandi-theme.el
@@ -108,12 +108,12 @@ which corresponds to a minimum contrast in relative luminance of
 
 ;;; Common accent backgrounds
 
-      (bg-red      "#ff8f88")
-      (bg-green    "#8adf80")
-      (bg-yellow   "#f3d000")
-      (bg-blue     "#bfc9ff")
-      (bg-magenta  "#dfa0f0")
-      (bg-cyan     "#a4d5f9")
+      (bg-red-intense     "#ff8f88")
+      (bg-green-intense   "#8adf80")
+      (bg-yellow-intense  "#f3d000")
+      (bg-blue-intense    "#bfc9ff")
+      (bg-magenta-intense "#dfa0f0")
+      (bg-cyan-intense    "#a4d5f9")
 
       (bg-red-subtle      "#ffcfbf")
       (bg-green-subtle    "#b3fabf")

modus-operandi-tinted-theme.el
@@ -107,12 +107,12 @@ which corresponds to a minimum contrast in relative luminance of
 
 ;;; Common accent backgrounds
 
-      (bg-red      "#ff8f88")
-      (bg-green    "#8adf80")
-      (bg-yellow   "#f3d000")
-      (bg-blue     "#bfc9ff")
-      (bg-magenta  "#dfa0f0")
-      (bg-cyan     "#a4d5f9")
+      (bg-red-intense     "#ff8f88")
+      (bg-green-intense   "#8adf80")
+      (bg-yellow-intense  "#f3d000")
+      (bg-blue-intense    "#bfc9ff")
+      (bg-magenta-intense "#dfa0f0")
+      (bg-cyan-intense    "#a4d5f9")
 
       (bg-red-subtle      "#ffcfbf")
       (bg-green-subtle    "#b3fabf")

modus-vivendi-deuteranopia-theme.el
@@ -109,12 +109,12 @@ standard).")
 
 ;;; Common accent backgrounds
 
-      (bg-red      "#9d1f1f")
-      (bg-green    "#2f822f")
-      (bg-yellow   "#7a6100")
-      (bg-blue     "#1640b0")
-      (bg-magenta  "#7030af")
-      (bg-cyan     "#2266ae")
+      (bg-red-intense     "#9d1f1f")
+      (bg-green-intense   "#2f822f")
+      (bg-yellow-intense  "#7a6100")
+      (bg-blue-intense    "#1640b0")
+      (bg-magenta-intense "#7030af")
+      (bg-cyan-intense    "#2266ae")
 
       (bg-red-subtle      "#620f2a")
       (bg-green-subtle    "#00422a")

modus-vivendi-theme.el
@@ -107,12 +107,12 @@ which corresponds to a minimum contrast in relative luminance of
 
 ;;; Common accent backgrounds
 
-      (bg-red      "#9d1f1f")
-      (bg-green    "#2f822f")
-      (bg-yellow   "#7a6100")
-      (bg-blue     "#1640b0")
-      (bg-magenta  "#7030af")
-      (bg-cyan     "#2266ae")
+      (bg-red-intense     "#9d1f1f")
+      (bg-green-intense   "#2f822f")
+      (bg-yellow-intense  "#7a6100")
+      (bg-blue-intense    "#1640b0")
+      (bg-magenta-intense "#7030af")
+      (bg-cyan-intense    "#2266ae")
 
       (bg-red-subtle      "#620f2a")
       (bg-green-subtle    "#00422a")

modus-vivendi-tinted-theme.el
@@ -107,12 +107,12 @@ which corresponds to a minimum contrast in relative luminance of
 
 ;;; Common accent backgrounds
 
-      (bg-red      "#9d1f1f")
-      (bg-green    "#2f822f")
-      (bg-yellow   "#7a6100")
-      (bg-blue     "#1640b0")
-      (bg-magenta  "#7030af")
-      (bg-cyan     "#2266ae")
+      (bg-red-intense     "#9d1f1f")
+      (bg-green-intense   "#2f822f")
+      (bg-yellow-intense  "#7a6100")
+      (bg-blue-intense    "#1640b0")
+      (bg-magenta-intense "#7030af")
+      (bg-cyan-intense    "#2266ae")
 
       (bg-red-subtle      "#620f2a")
       (bg-green-subtle    "#00422a")

commit 9a7d47b9be419f0b7d18808881f6f9a8f4524f07

Use correct names for coloured backgrounds
This is a regression caused by commit 7c74495.

Thanks to Aleksei Pirogov for reporting the problem in issue 55 on the
GitHub mirror: <https://github.com/protesilaos/modus-themes/issues/55>.

doc/modus-themes.info
@@ -4401,36 +4401,36 @@ Contributions to code or documentation
      Xinglu Chen, Yuanchen Xie, okamsn.
 
 Ideas and user feedback
-     Aaron Jensen, Adam Porter, Adam Spiers, Adrian Manea, Alex Griffin,
-     Alex Koen, Alex Peitsinis, Alexey Shmalko, Alok Singh, Anders
-     Johansson, André Alexandre Gomes, Andrew Tropin, Antonio Hernández
-     Blas, Arif Rezai, Augusto Stoffel, Basil L. Contovounesios, Burgess
-     Chang, Christian Tietze, Christopher Dimech, Christopher League,
-     Damien Cassou, Daniel Mendler, Dario Gjorgjevski, David Edmondson,
-     Davor Rotim, Divan Santana, Eliraz Kedmi, Emanuele Michele Alberto
-     Monterosso, Farasha Euker, Feng Shu, Gautier Ponsinet, Gerry
-     Agbobada, Gianluca Recchia, Gonçalo Marrafa, Guilherme Semente,
-     Gustavo Barros, Hörmetjan Yiltiz, Ilja Kocken, Iris Garcia, Ivan
-     Popovych, James Ferguson, Jeremy Friesen, Jerry Zhang, Johannes
-     Grødem, John Haman, Jonas Collberg, Jorge Morais, Joshua O’Connor,
-     Julio C.  Villasante, Kenta Usami, Kevin Fleming, Kévin Le Gouguec,
-     Kevin Kainan Li, Kostadin Ninev, Laith Bahodi, Len Trigg, Lennart
-     C.  Karssen, Luis Miguel Castañeda, Magne Hov, Manuel Uberti, Mark
-     Bestley, Mark Burton, Mark Simpson, Markus Beppler, Matt Armstrong,
-     Matthias Fuchs, Mattias Engdegård, Mauro Aranda, Maxime Tréca,
-     Michael Goldenberg, Morgan Smith, Morgan Willcock, Murilo Pereira,
-     Nicky van Foreest, Nicolas De Jaeghere, Pablo Stafforini, Paul
-     Poloskov, Pengji Zhang, Pete Kazmier, Peter Wu, Philip Kaludercic,
-     Pierre Téchoueyres, Przemysław Kryger, Robert Hepple, Roman
-     Rudakov, Ryan Phillips, Rytis Paškauskas, Rudolf Adamkovič, Sam
-     Kleinman, Samuel Culpepper, Saša Janiška, Shreyas Ragavan, Simon
-     Pugnet, Tassilo Horn, Thibaut Verron, Thomas Heartman, Togan
-     Muftuoglu, Tony Zorman, Trey Merkley, Tomasz Hołubowicz, Toon
-     Claes, Uri Sharf, Utkarsh Singh, Vincent Foley.  As well as users:
-     Ben, CsBigDataHub1, Emacs Contrib, Eugene, Fourchaux, Fredrik,
-     Moesasji, Nick, Summer Emacs, TheBlob42, Trey, bepolymathe,
-     bit9tream, derek-upham, doolio, fleimgruber, gitrj95, iSeeU,
-     jixiuf, okamsn, pRot0ta1p, wakamenod.
+     Aaron Jensen, Adam Porter, Adam Spiers, Adrian Manea, Aleksei
+     Pirogov, Alex Griffin, Alex Koen, Alex Peitsinis, Alexey Shmalko,
+     Alok Singh, Anders Johansson, André Alexandre Gomes, Andrew Tropin,
+     Antonio Hernández Blas, Arif Rezai, Augusto Stoffel, Basil L.
+     Contovounesios, Burgess Chang, Christian Tietze, Christopher
+     Dimech, Christopher League, Damien Cassou, Daniel Mendler, Dario
+     Gjorgjevski, David Edmondson, Davor Rotim, Divan Santana, Eliraz
+     Kedmi, Emanuele Michele Alberto Monterosso, Farasha Euker, Feng
+     Shu, Gautier Ponsinet, Gerry Agbobada, Gianluca Recchia, Gonçalo
+     Marrafa, Guilherme Semente, Gustavo Barros, Hörmetjan Yiltiz, Ilja
+     Kocken, Iris Garcia, Ivan Popovych, James Ferguson, Jeremy Friesen,
+     Jerry Zhang, Johannes Grødem, John Haman, Jonas Collberg, Jorge
+     Morais, Joshua O’Connor, Julio C.  Villasante, Kenta Usami, Kevin
+     Fleming, Kévin Le Gouguec, Kevin Kainan Li, Kostadin Ninev, Laith
+     Bahodi, Len Trigg, Lennart C.  Karssen, Luis Miguel Castañeda,
+     Magne Hov, Manuel Uberti, Mark Bestley, Mark Burton, Mark Simpson,
+     Markus Beppler, Matt Armstrong, Matthias Fuchs, Mattias Engdegård,
+     Mauro Aranda, Maxime Tréca, Michael Goldenberg, Morgan Smith,
+     Morgan Willcock, Murilo Pereira, Nicky van Foreest, Nicolas De
+     Jaeghere, Pablo Stafforini, Paul Poloskov, Pengji Zhang, Pete
+     Kazmier, Peter Wu, Philip Kaludercic, Pierre Téchoueyres,
+     Przemysław Kryger, Robert Hepple, Roman Rudakov, Ryan Phillips,
+     Rytis Paškauskas, Rudolf Adamkovič, Sam Kleinman, Samuel Culpepper,
+     Saša Janiška, Shreyas Ragavan, Simon Pugnet, Tassilo Horn, Thibaut
+     Verron, Thomas Heartman, Togan Muftuoglu, Tony Zorman, Trey
+     Merkley, Tomasz Hołubowicz, Toon Claes, Uri Sharf, Utkarsh Singh,
+     Vincent Foley.  As well as users: Ben, CsBigDataHub1, Emacs
+     Contrib, Eugene, Fourchaux, Fredrik, Moesasji, Nick, Summer Emacs,
+     TheBlob42, Trey, bepolymathe, bit9tream, derek-upham, doolio,
+     fleimgruber, gitrj95, iSeeU, jixiuf, okamsn, pRot0ta1p, wakamenod.
 
 Packaging
      Basil L. Contovounesios, Eli Zaretskii, Glenn Morris, Mauro Aranda,
@@ -5164,11 +5164,11 @@ Node: Sources of the themes172317
 Node: Issues you can help with173212
 Node: Patches require copyright assignment to the FSF174603
 Node: Acknowledgements176823
-Node: GNU Free Documentation License180531
-Node: Indices205895
-Node: Function index206074
-Node: Variable index207114
-Node: Concept index209013
+Node: GNU Free Documentation License180548
+Node: Indices205912
+Node: Function index206091
+Node: Variable index207131
+Node: Concept index209030
 
 End Tag Table
 

doc/modus-themes.org
@@ -4290,35 +4290,35 @@ The Modus themes are a collective effort.  Every bit of work matters.
   Xie, okamsn.
 
 + Ideas and user feedback :: Aaron Jensen, Adam Porter, Adam Spiers,
-  Adrian Manea, Alex Griffin, Alex Koen, Alex Peitsinis, Alexey
-  Shmalko, Alok Singh, Anders Johansson, André Alexandre Gomes, Andrew
-  Tropin, Antonio Hernández Blas, Arif Rezai, Augusto Stoffel, Basil
-  L.{{{space()}}} Contovounesios, Burgess Chang, Christian Tietze,
-  Christopher Dimech, Christopher League, Damien Cassou, Daniel
-  Mendler, Dario Gjorgjevski, David Edmondson, Davor Rotim, Divan
-  Santana, Eliraz Kedmi, Emanuele Michele Alberto Monterosso, Farasha
-  Euker, Feng Shu, Gautier Ponsinet, Gerry Agbobada, Gianluca Recchia,
-  Gonçalo Marrafa, Guilherme Semente, Gustavo Barros, Hörmetjan
-  Yiltiz, Ilja Kocken, Iris Garcia, Ivan Popovych, James Ferguson,
-  Jeremy Friesen, Jerry Zhang, Johannes Grødem, John Haman, Jonas
-  Collberg, Jorge Morais, Joshua O'Connor, Julio C. Villasante, Kenta
-  Usami, Kevin Fleming, Kévin Le Gouguec, Kevin Kainan Li, Kostadin
-  Ninev, Laith Bahodi, Len Trigg, Lennart C. Karssen, Luis Miguel
-  Castañeda, Magne Hov, Manuel Uberti, Mark Bestley, Mark Burton, Mark
-  Simpson, Markus Beppler, Matt Armstrong, Matthias Fuchs, Mattias
-  Engdegård, Mauro Aranda, Maxime Tréca, Michael Goldenberg, Morgan
-  Smith, Morgan Willcock, Murilo Pereira, Nicky van Foreest, Nicolas
-  De Jaeghere, Pablo Stafforini, Paul Poloskov, Pengji Zhang, Pete
-  Kazmier, Peter Wu, Philip Kaludercic, Pierre Téchoueyres, Przemysław
-  Kryger, Robert Hepple, Roman Rudakov, Ryan Phillips, Rytis
-  Paškauskas, Rudolf Adamkovič, Sam Kleinman, Samuel Culpepper, Saša
-  Janiška, Shreyas Ragavan, Simon Pugnet, Tassilo Horn, Thibaut
-  Verron, Thomas Heartman, Togan Muftuoglu, Tony Zorman, Trey Merkley,
-  Tomasz Hołubowicz, Toon Claes, Uri Sharf, Utkarsh Singh, Vincent
-  Foley.  As well as users: Ben, CsBigDataHub1, Emacs Contrib, Eugene,
-  Fourchaux, Fredrik, Moesasji, Nick, Summer Emacs, TheBlob42, Trey,
-  bepolymathe, bit9tream, derek-upham, doolio, fleimgruber, gitrj95,
-  iSeeU, jixiuf, okamsn, pRot0ta1p, wakamenod.
+  Adrian Manea, Aleksei Pirogov, Alex Griffin, Alex Koen, Alex
+  Peitsinis, Alexey Shmalko, Alok Singh, Anders Johansson, André
+  Alexandre Gomes, Andrew Tropin, Antonio Hernández Blas, Arif Rezai,
+  Augusto Stoffel, Basil L.{{{space()}}} Contovounesios, Burgess
+  Chang, Christian Tietze, Christopher Dimech, Christopher League,
+  Damien Cassou, Daniel Mendler, Dario Gjorgjevski, David Edmondson,
+  Davor Rotim, Divan Santana, Eliraz Kedmi, Emanuele Michele Alberto
+  Monterosso, Farasha Euker, Feng Shu, Gautier Ponsinet, Gerry
+  Agbobada, Gianluca Recchia, Gonçalo Marrafa, Guilherme Semente,
+  Gustavo Barros, Hörmetjan Yiltiz, Ilja Kocken, Iris Garcia, Ivan
+  Popovych, James Ferguson, Jeremy Friesen, Jerry Zhang, Johannes
+  Grødem, John Haman, Jonas Collberg, Jorge Morais, Joshua O'Connor,
+  Julio C. Villasante, Kenta Usami, Kevin Fleming, Kévin Le Gouguec,
+  Kevin Kainan Li, Kostadin Ninev, Laith Bahodi, Len Trigg, Lennart
+  C. Karssen, Luis Miguel Castañeda, Magne Hov, Manuel Uberti, Mark
+  Bestley, Mark Burton, Mark Simpson, Markus Beppler, Matt Armstrong,
+  Matthias Fuchs, Mattias Engdegård, Mauro Aranda, Maxime Tréca,
+  Michael Goldenberg, Morgan Smith, Morgan Willcock, Murilo Pereira,
+  Nicky van Foreest, Nicolas De Jaeghere, Pablo Stafforini, Paul
+  Poloskov, Pengji Zhang, Pete Kazmier, Peter Wu, Philip Kaludercic,
+  Pierre Téchoueyres, Przemysław Kryger, Robert Hepple, Roman Rudakov,
+  Ryan Phillips, Rytis Paškauskas, Rudolf Adamkovič, Sam Kleinman,
+  Samuel Culpepper, Saša Janiška, Shreyas Ragavan, Simon Pugnet,
+  Tassilo Horn, Thibaut Verron, Thomas Heartman, Togan Muftuoglu, Tony
+  Zorman, Trey Merkley, Tomasz Hołubowicz, Toon Claes, Uri Sharf,
+  Utkarsh Singh, Vincent Foley.  As well as users: Ben, CsBigDataHub1,
+  Emacs Contrib, Eugene, Fourchaux, Fredrik, Moesasji, Nick, Summer
+  Emacs, TheBlob42, Trey, bepolymathe, bit9tream, derek-upham, doolio,
+  fleimgruber, gitrj95, iSeeU, jixiuf, okamsn, pRot0ta1p, wakamenod.
 
 + Packaging :: Basil L.{{{space()}}} Contovounesios, Eli Zaretskii,
   Glenn Morris, Mauro Aranda, Richard Stallman, Stefan Kangas (core

modus-themes.el
@@ -808,10 +808,10 @@ represents."
 
 (defvar modus-themes-preset-overrides-faint
   '((bg-completion       bg-inactive)
-    (bg-hover            bg-cyan)
+    (bg-hover            bg-cyan-subtle)
     (bg-hover-secondary  bg-magenta-subtle)
     (bg-hl-line          bg-dim)
-    (bg-paren-match      bg-cyan)
+    (bg-paren-match      bg-cyan-subtle)
     (bg-region           bg-active)
 
     (bg-mode-line-active        bg-inactive)
@@ -913,8 +913,8 @@ Info node `(modus-themes) Option for palette overrides'.")
   '((bg-region bg-cyan)
 
     (bg-completion       bg-cyan-subtle)
-    (bg-hover            bg-yellow)
-    (bg-hover-secondary  bg-magenta)
+    (bg-hover            bg-yellow-intense)
+    (bg-hover-secondary  bg-magenta-intense)
     (bg-hl-line          bg-cyan-subtle)
 
     (bg-mode-line-active      bg-blue-subtle)