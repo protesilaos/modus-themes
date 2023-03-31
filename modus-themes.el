081093922d36b920b4e8a0ba0d5e70d1cefbf643
Protesilaos Stavrou <info@protesilaos.com>
2023-03-31 15:10:02 +0300

    Use a subtle 3D effect for clickable buttons
    I realised the flat style creates ambiguity between the button and the
    text fields.  This happens, for example, with M-x customize-variable
    for 'org-capture-templates' which has lots of button and text field
    combinations.  The added sense of depth helps with the usability of
    these buttons because it makes them unambiguous.

--- modus-themes.el
+++ modus-themes.el
@@ -1587,7 +1587,10 @@ FG and BG are the main colors."
     `(modus-themes-slant ((,c ,@(modus-themes--slant))))
     `(modus-themes-ui-variable-pitch ((,c ,@(modus-themes--variable-pitch-ui))))
 ;;;;; other custom faces
-    `(modus-themes-button ((,c :inherit variable-pitch :box ,border :background ,bg-button-active :foreground ,fg-button-active)))
+    `(modus-themes-button ((,c :inherit variable-pitch
+                               :box (:line-width 1 :color ,border :style released-button)
+                               :background ,bg-button-active
+                               :foreground ,fg-button-active)))
     `(modus-themes-key-binding ((,c :inherit (bold modus-themes-fixed-pitch) :foreground ,keybind)))
     `(modus-themes-prompt ((,c ,@(modus-themes--prompt fg-prompt bg-prompt))))
     `(modus-themes-reset-soft ((,c :background ,bg-main :foreground ,fg-main
