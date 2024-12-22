d79e658660cf5e866c535ad684bc5758277ebdf7
Protesilaos Stavrou <info@protesilaos.com>
2024-12-22 11:10:10 +0200

    Make Ivy subdir and virtual easier to tell apart from the completion candidates

--- modus-themes.el
+++ modus-themes.el
@@ -2804,8 +2804,8 @@ FG and BG are the main colors."
     `(ivy-minibuffer-match-face-4 ((,c :inherit modus-themes-completion-match-2)))
     `(ivy-remote ((,c :inherit italic)))
     `(ivy-separator ((,c :inherit shadow)))
-    `(ivy-subdir ((,c :foreground ,accent-0)))
-    `(ivy-virtual ((,c :foreground ,accent-1)))
+    `(ivy-subdir ((,c :foreground ,keyword)))
+    `(ivy-virtual ((,c :foreground ,warning)))
 ;;;;; ivy-posframe
     `(ivy-posframe-border ((,c :background ,border)))
     `(ivy-posframe-cursor ((,c :background ,fg-main :foreground ,bg-main)))
