063ae646f0e058cf67bc3ea9d3510519420d27b7
Protesilaos Stavrou <info@protesilaos.com>
2025-04-15 08:14:40 +0300

    Refine 'green-warmer' in modus-vivendi-tinted

--- modus-vivendi-tinted-theme.el
+++ modus-vivendi-tinted-theme.el
@@ -74,7 +74,7 @@ which corresponds to a minimum contrast in relative luminance of
       (red-faint       "#ef8386")
       (red-intense     "#ff5f5f")
       (green           "#44bc44")
-      (green-warmer    "#75c12b")
+      (green-warmer    "#75c13e")
       (green-cooler    "#00c06f")
       (green-faint     "#88ca9f")
       (green-intense   "#44df44")
