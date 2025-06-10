6979a37a1e5489d213c82aa33c75001724fc32fd
Protesilaos Stavrou <info@protesilaos.com>
2025-06-10 09:40:40 +0300

    Add support for the header-line-inactive face (Emacs 31)

--- modus-themes.el
+++ modus-themes.el
@@ -1689,6 +1689,7 @@ FG and BG are the main colors."
     `(escape-glyph ((,c :foreground ,err)))
     `(file-name-shadow ((,c :inherit shadow)))
     `(header-line ((,c :inherit modus-themes-ui-variable-pitch :background ,bg-dim)))
+    `(header-line-inactive ((,c :inherit (modus-themes-ui-variable-pitch shadow))))
     `(header-line-highlight ((,c :background ,bg-hover :foreground ,fg-main :box ,fg-main)))
     `(help-argument-name ((,c :inherit modus-themes-slant :foreground ,variable)))
     `(help-key-binding ((,c :inherit modus-themes-key-binding)))
