92109f871b53dc00b30c78e279e703cb0d787fa1
Protesilaos Stavrou <info@protesilaos.com>
2025-09-09 09:55:14 +0300

    Make calendar-today easier to spot

--- modus-themes.el
+++ modus-themes.el
@@ -1917,7 +1917,10 @@ FG and BG are the main colors."
     `(bookmark-menu-bookmark ((,c :inherit bold)))
 ;;;;; calendar and diary
     `(calendar-month-header ((,c :inherit bold)))
-    `(calendar-today ((,c :inherit bold :underline t)))
+    `(calendar-today
+      ((default :foreground ,date-common :inverse-video t)
+       (((supports :box t))
+        :box (:line-width (-1 . -1) :color ,fg-main))))
     `(calendar-weekday-header ((,c :foreground ,date-weekday)))
     `(calendar-weekend-header ((,c :foreground ,date-weekend)))
     `(diary ((,c :foreground ,date-common)))
