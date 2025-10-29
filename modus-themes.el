8803f696cd88979aa3484c1f458bb917af98e568
Protesilaos Stavrou <info@protesilaos.com>
2025-10-29 05:41:36 +0200

    Update the Commentary of all the files

--- modus-themes.el
+++ modus-themes.el
@@ -28,9 +28,10 @@
 ;;
 ;; The Modus themes conform with the highest standard for
 ;; color-contrast accessibility between background and foreground
-;; values (WCAG AAA).  Please refer to the official Info manual for
-;; further documentation (distributed with the themes, or available
-;; at: <https://protesilaos.com/emacs/modus-themes>).
+;; values (WCAG AAA).  They are also highly customizable and can even
+;; be used as a basis for other themes.  Please refer to the official
+;; Info manual for further documentation (distributed with the themes,
+;; or available at: <https://protesilaos.com/emacs/modus-themes>).
 
 ;;; Code:
 
