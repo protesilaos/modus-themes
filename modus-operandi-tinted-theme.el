3f3655451bdb68ebba707450224dd2e09ba2589e
Protesilaos Stavrou <info@protesilaos.com>
2025-11-01 07:03:21 +0200

    Make a small clarification to the Commentary of each file

--- modus-operandi-tinted-theme.el
+++ modus-operandi-tinted-theme.el
@@ -27,7 +27,7 @@
 ;; The Modus themes conform with the highest standard for
 ;; color-contrast accessibility between background and foreground
 ;; values (WCAG AAA).  They are also highly customizable and can even
-;; be used as a basis for other themes.  Please refer to the official
+;; be used as the basis for other themes.  Please refer to the official
 ;; Info manual for further documentation (distributed with the themes,
 ;; or available at: <https://protesilaos.com/emacs/modus-themes>).
 
